8d9c6ee8cf695fb85ccbc87e0f0d1104baa7d7f1
muchen <75168572@qq.com>
2017-10-25 21:19:17 +0800

    购物车页面部分数据渲染，全选/反选功能完成

--- src/components/goods/cart.vue
+++ src/components/goods/cart.vue
@@ -46,7 +46,7 @@
                             <tbody>
                                 <tr>
                                     <th width="48" align="center">
-                                        <a>全选</a>
+                                        <el-switch v-model="isselectedAll" on-text="反选" off-text="全选" @change="isSelectedAlla"></el-switch>
                                     </th>
                                     <th align="left" colspan="2">商品信息</th>
                                     <th width="84" align="left">单价</th>
@@ -54,7 +54,7 @@
                                     <th width="104" align="left">金额(元)</th>
                                     <th width="54" align="center">操作</th>
                                 </tr>
-                                <tr v-if="">
+                                <tr v-if="goodsList.length<=0">
                                     <td colspan="10">
                                         <div class="msg-tips">
                                             <div class="icon warning">
@@ -68,6 +68,21 @@
                                         </div>
                                     </td>
                                 </tr>
+                                <tr v-for="(item,index) in goodsList" :key="item.id" class="goodslisttable" >
+                                    <td>
+                                        <el-switch v-model="values[index]" on-text="取消" off-text="选择" @change="isSelected">
+                                        </el-switch>
+                                    </td>
+                                    <td align="left" colspan="2">{{item.title}}</td>
+                                    <td  width="84">{{item.sell_price}}</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td><el-button type="success">删除</el-button></td>
+                                     
+                                    
+                                </tr>
+
+
                                 <tr>
                                     <th align="right" colspan="8">
                                         已选择商品
@@ -96,34 +111,64 @@
 
 <script>
 
-    import {getItem} from "../../../static/kits/localstorageKit.js"
+    import { getItem } from "../../../static/kits/localstorageKit.js"
     export default {
         data() {
             return {
-                goodsList:[]
+                goodsList: [],
+                values:[],
+                isselectedAll:false,
+                
+
             }
         },
-        created(){
+        created() {
             this.getList()
         },
         methods: {
-            
-            getList(){
+            isSelected(){
+                for(var i=0;i<this.values.length;i++){
+                        if(this.values[i]==false){
+                                this.isselectedAll = false;
+                        }
+                    }
+            },
+            //全选按钮
+            isSelectedAlla(){
+                // console.log('isSelectedAlla')
+                    for(var i=0;i<this.values.length;i++){
+                        if(this.isselectedAll==true){
+                                this.values[i] = true;
+                        }else if(this.isselectedAll==false){
+                            this.values[i] = false;
+                        }
+                    }
+            },
+            getList() {
                 var goodsObj = getItem();
                 var idsArr = []
-                for (var key in goodsObj){
+                for (var key in goodsObj) {
                     idsArr.push(key);
                 }
                 var ids = idsArr.join(",")
-                this.$http.get("/site/comment/getshopcargoods/"+ids)
-                .then(res=>{
-                    this.goodsList = res.data.message
-                    
-                    console.log(this.goodsList)
-                })
+                this.$http.get("/site/comment/getshopcargoods/" + ids)
+                    .then(res => {
+                        this.goodsList = res.data.message
+
+                        //根据返回的数组个数初始化values数组的个数
+                        this.goodsList.forEach((item,index)=>{
+                            this.values.push(false);
+
+                            
+                            item.buycount = goodsObj[item.id]
+                        })
+                    })
             }
         }
     }
 </script>
 <style scoped>
+    .goodslisttable td{
+        /* text-align: center; */
+    }
 </style>
